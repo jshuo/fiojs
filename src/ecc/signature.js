391e499e3fc4c33fdf0588f644135c0583035979
Jeff Shuo <jeffshuo@gmail.com>
2022-05-23 00:37:04 +0800

    fio trx ok

--- src/ecc/signature.js
+++ src/ecc/signature.js
@@ -8,6 +8,109 @@ const PublicKey = require('./key_public');
 const PrivateKey = require('./key_private');
 
 module.exports = Signature
+const toBip32StringPath = (path) => `m/${fromHardened(path[0])}'/${fromHardened(path[1])}'/${fromHardened(path[2])}'/${path[3]}/${path[4]}`
+const HD_HARDENED = 0x80000000
+const fromHardened = (n) => (n & ~HD_HARDENED) >>> 0
+
+function splitPath(path) {
+  const elements = path.split('/')
+  const pathLen = elements.length
+  if (pathLen < 2 || pathLen > 6) throw Error('Invalid Path, only support 1 to 5 depth path')
+
+  const pathProps = {}
+  pathProps.pathNum = pathLen - 1
+  elements.forEach((element, index) => {
+    if (index === 0) return
+    const props = {}
+    const isHardened = element.length > 1 && element[element.length - 1] === "'"
+    if (isHardened) {
+      props.value = parseInt(element.slice(0, -1), 10)
+    } else {
+      props.value = parseInt(element, 10)
+    }
+    props.isHardened = isHardened
+    props.depth = index
+    switch (index) {
+      case 1:
+        pathProps.purpose = props
+        break
+      case 2:
+        pathProps.coinType = props
+        break
+      case 3:
+        pathProps.accountId = props
+        break
+      case 4:
+        pathProps.change = props
+        break
+      case 5:
+        pathProps.addressIndex = props
+        break
+    }
+  })
+  return pathProps
+}
+
+const HARDENED_OFFSET = 0x80000000
+function buildPathBuffer(path, num) {
+  const getHardenedValue = (pathLevel) => {
+    if (pathLevel && pathLevel.isHardened) return pathLevel.value + HARDENED_OFFSET
+    else if (pathLevel && !pathLevel.isHardened) return pathLevel.value
+    else throw Error('Build path error')
+  }
+  const pathProps = splitPath(path)
+  let pathNum = num && num >= 1 && num < 6 ? num : pathProps.pathNum
+  const buf = Buffer.alloc(4 * pathNum)
+  const { purpose, coinType, accountId, change, addressIndex } = pathProps
+  for (let i = 0; i < pathNum; i++) {
+    // buffer need to start from 0 bytes
+    switch (i) {
+      case 0:
+        buf.writeUInt32LE(getHardenedValue(purpose), i * 4)
+        break
+      case 1:
+        buf.writeUInt32LE(getHardenedValue(coinType), i * 4)
+        break
+      case 2:
+        buf.writeUInt32LE(getHardenedValue(accountId), i * 4)
+        break
+      case 3:
+        buf.writeUInt32LE(getHardenedValue(change), i * 4)
+        break
+      case 4:
+        buf.writeUInt32LE(getHardenedValue(addressIndex), i * 4)
+        break
+    }
+  }
+  return { pathNum, pathBuffer: buf }
+}
+
+function buildTxBuffer(paths, txs, tp, chainId) {
+  if (paths.length != txs.length) throw Error('Inconsistent length of paths and txs')
+
+  const head = [],
+    data = []
+  for (let i = 0; i < paths.length; i++) {
+    const headerBuffer = Buffer.alloc(4)
+    headerBuffer.writeUInt16LE(tp, 0)
+    headerBuffer.writeUInt16LE(chainId, 2)
+
+    const path = paths[i]
+    const { pathNum, pathBuffer } = buildPathBuffer(path)
+    // generic prepare can use 3 or 5 path level key to sign
+    if (pathNum !== 5 && pathNum !== 3) throw Error('Invalid Path for Signing Transaction')
+    //@ts-ignore
+    head.push(Buffer.concat([Buffer.from([pathNum * 4 + 4]), headerBuffer, pathBuffer]))
+
+    // fixed 2 byte length
+    const preparedTxLenBuf = Buffer.alloc(2)
+    preparedTxLenBuf.writeUInt16BE(txs[i].length, 0)
+    //@ts-ignore
+    data.push(Buffer.concat([preparedTxLenBuf, txs[i]]))
+  }
+
+  return Buffer.concat([Buffer.from([paths.length]), ...head, ...data])
+}
 
 function Signature(r, s, i) {
     assert.equal(r != null, true, 'Missing parameter');
@@ -174,7 +277,7 @@ function Signature(r, s, i) {
 
     @return {Signature}
 */
-Signature.sign = function(data, privateKey, transport, encoding = 'utf8') {
+Signature.sign = function(data, privateKey, encoding = 'utf8') {
     if(typeof data === 'string') {
         data = Buffer.from(data, encoding)
     }
@@ -221,6 +324,19 @@ Signature.signHash = function(dataSha256, privateKey, encoding = 'hex') {
         console.log("WARN: " + nonce + " attempts to find canonical signature");
       }
     }
+
+    const SIGNATURE_LENGTH = 65
+    const hashedTx = []
+    const FIO_ACCOUNT_PATH = `m/44'/235'/0'/0/0`
+    hashedTx.push(dataSha256)
+    const txBuffer = buildTxBuffer([toBip32StringPath(FIO_ACCOUNT_PATH)], hashedTx)
+    // if (transport) {
+    //   const rsp = await transport.Send(0x70, 0xa4, 0, 0, Buffer.concat([txBuffer]))
+    //   if (rsp.status !== StatusCode.SUCCESS) throw new TransportStatusError(rsp.status)
+    //   if (rsp.dataLength !== SIGNATURE_LENGTH) throw Error('Invalid length Signature')
+    // }
+
+
     return Signature.fromBuffer(Buffer.concat([Buffer.from(i.toString(16), 'hex'), ecsignature.r.toBuffer(), ecsignature.s.toBuffer()]))
 };
 
